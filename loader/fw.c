265f4012fd88019d02270d0b5997b5983a1a9600
Rohith Gorle <gorlerohithsriram@gmail.com>
2026-07-11 07:33:26 +0000

    Enhance firewall command options and help output
    Updated command handling in the firewall program to include 'allow' and 'unallow' commands, along with improved help output.

--- loader/fw.c
+++ loader/fw.c
@@ -1,47 +1,65 @@
+#include "../bpf/firewall.skel.h"
+#include <net/if.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-void print_help(void)
+#include <bpf/libbpf.h>
+#include <bpf/bpf.h>
+
+#define BLOCK_MAP_PATH "/sys/fs/bpf/blocked_ips"
+#define ALLOW_MAP_PATH "/sys/fs/bpf/allowed_ips"
+
+#define VALUE_PRESENT 1
+
+static struct firewall_bpf *skel = NULL;
+static struct bpf_link *xdp_link = NULL;
+
+static const char *iface = "enp0s3";
+
+static void print_help(void)
 {
+    printf("\n");
     printf("eBPF Firewall\n\n");
 
     printf("Usage:\n");
-    printf("  sudo ./fw load\n");
-    printf("  sudo ./fw unload\n");
-    printf("  sudo ./fw block <ip>\n");
-    printf("  sudo ./fw unblock <ip>\n");
-    printf("  sudo ./fw status\n");
-    printf("  sudo ./fw help\n");
+    printf(" sudo ./fw load\n");
+    printf(" sudo ./fw unload\n");
+    printf(" sudo ./fw block <ip>\n");
+    printf(" sudo ./fw unblock <ip>\n");
+    printf(" sudo ./fw allow <ip>\n");
+    printf(" sudo ./fw unallow <ip>\n");
+    printf(" sudo ./fw status\n");
+    printf(" sudo ./fw help\n");
 }
 
 int cmd_load(void)
 {
-    printf("Loading firewall...\n");
-    return 0;
-}
+    int err;
 
-int cmd_unload(void)
-{
-    printf("Unloading firewall...\n");
-    return 0;
-}
+    skel = firewall_bpf__open();
 
-int cmd_block(char *ip)
-{
-    printf("Blocking IP: %s\n", ip);
-    return 0;
-}
+    if (!skel)
+    {
+        fprintf(stderr, "Failed to open BPF skeleton\n");
+        return 1;
+    }
 
-int cmd_unblock(char *ip)
-{
-    printf("Unblocking IP: %s\n", ip);
-    return 0;
-}
+    err = firewall_bpf__load(skel);
 
-int cmd_status(void)
-{
-    printf("Firewall Status\n");
+    if (err)
+    {
+        fprintf(stderr, "Failed to load BPF program\n");
+        firewall_bpf__destroy(skel);
+        skel = NULL;
+        return 1;
+    }
+
+    printf("Firewall program loaded successfully.\n");
+
     return 0;
 }
 
@@ -54,16 +72,27 @@ int main(int argc, char *argv[])
     }
 
     if (!strcmp(argv[1], "help"))
-        return print_help(), 0;
+    {
+        print_help();
+        return 0;
+    }
 
     if (!strcmp(argv[1], "load"))
-        return cmd_load();
+    {
+    return cmd_load();
+    }
 
     if (!strcmp(argv[1], "unload"))
-        return cmd_unload();
+    {
+        printf("Unloading firewall...\n");
+        return 0;
+    }
 
     if (!strcmp(argv[1], "status"))
-        return cmd_status();
+    {
+        printf("Firewall Status\n");
+        return 0;
+    }
 
     if (!strcmp(argv[1], "block"))
     {
@@ -73,7 +102,8 @@ int main(int argc, char *argv[])
             return 1;
         }
 
-        return cmd_block(argv[2]);
+        printf("Blocking %s\n", argv[2]);
+        return 0;
     }
 
     if (!strcmp(argv[1], "unblock"))
@@ -84,7 +114,32 @@ int main(int argc, char *argv[])
             return 1;
         }
 
-        return cmd_unblock(argv[2]);
+        printf("Unblocking %s\n", argv[2]);
+        return 0;
+    }
+
+    if (!strcmp(argv[1], "allow"))
+    {
+        if (argc != 3)
+        {
+            printf("Usage: sudo ./fw allow <ip>\n");
+            return 1;
+        }
+
+        printf("Allowing %s\n", argv[2]);
+        return 0;
+    }
+
+    if (!strcmp(argv[1], "unallow"))
+    {
+        if (argc != 3)
+        {
+            printf("Usage: sudo ./fw unallow <ip>\n");
+            return 1;
+        }
+
+        printf("Removing %s from allow list\n", argv[2]);
+        return 0;
     }
 
     printf("Unknown command\n");
